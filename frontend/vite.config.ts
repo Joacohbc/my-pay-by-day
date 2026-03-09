0628300722ff43b2e5d7b9c7d2887f7f90c4023d
Joacohbc <69821825+Joacohbc@users.noreply.github.com>
2026-03-09 17:55:19 +0000

    feat: update PWA assets and configuration
    - Changed favicon from PNG to ICO format and updated apple-touch-icon to a new size.
    - Added new PWA asset images including various sizes and a maskable icon.
    - Introduced a new configuration file for PWA assets generation.
    - Updated Vite configuration to reference new PWA icons and removed deprecated icon references.
    - Added dependency for PWA assets generator in package.json and pnpm-lock.yaml.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -20,9 +20,10 @@ export default defineConfig({
         background_color: '#1B1E23',
         theme_color: '#1B1E23',
         icons: [
-          { src: '/icon-192.png', sizes: '192x192', type: 'image/png' },
-          { src: '/icon-512.png', sizes: '512x512', type: 'image/png' },
-          { src: '/icon-512.png', sizes: '512x512', type: 'image/png', purpose: 'maskable' },
+          { src: 'pwa-64x64.png', sizes: '64x64', type: 'image/png' },
+          { src: 'pwa-192x192.png', sizes: '192x192', type: 'image/png' },
+          { src: 'pwa-512x512.png', sizes: '512x512', type: 'image/png' },
+          { src: 'maskable-icon-512x512.png', sizes: '512x512', type: 'image/png', purpose: 'maskable' },
         ],
       },
       workbox: {
