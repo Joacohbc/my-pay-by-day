f48aa6edd8f317d3fb2ef8b507be5e3995e00486
Joacohbc <69821825+Joacohbc@users.noreply.github.com>
2026-03-09 12:06:57 +0000

    feat: add Dockerfile for frontend build and run script for backend

--- run-native.sh
+++ run-native.sh
@@ -2,4 +2,9 @@ docker run --rm -p 8080:8080 \
     -v "$(pwd)/data:/work/data" \
     -e "DB_FIELD_ENCRYPTION_KEY=una-frase-larga-y-aleatoria-de-mas-de-32-chars" \
     -e "QUARKUS_DATASOURCE_JDBC_URL=jdbc:sqlite:/work/data/mypaybyday.db" \
-    quarkus/mypaybyday
+    mypaybyday/backend
+
+docker build -f src/main/docker/Dockerfile.native-multistage -t mypaybyday/backend .
+
+docker build -f Dockerfile -t mypaybyday/frontend .
+docker run  -p 8081:80 mypaybyday/frontend
